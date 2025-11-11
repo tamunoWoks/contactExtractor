7e2244ea645eeade1bd8de24a3af3624368c4613
Tammy Wokoma <tamunosakiwokoma86@gmail.com>
2025-11-11 12:39:29 -0700

    Add email regex pattern to contactExtractor.py

--- contactExtractor.py
+++ contactExtractor.py
@@ -9,3 +9,11 @@ phone_re = re.compile(r'''(
     (\d{4})                      # Last four digits
     (\s*(ext|x|ext\.)\s*(\d{2,5}))? # Extension (optional)
 )''', re.VERBOSE)
+
+# Create email regex
+email_re = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+       # Username
+    @                       # @ symbol
+    [a-zA-Z0-9.-]+          # Domain name
+    (\.[a-zA-Z]{2,4})       # Dot-something
+)''', re.VERBOSE)
